190773e8c0c5ae56070ad1fe9497a8e86ee0a5d1
damian-balas <damian.webdev@gmail.com>
2019-10-30 16:04:10 +0100

    Updates

---
+++ precache-manifest.0b7890c77b02d856e00dd9f53b59bca5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc581ce4011ef283406b9709751ad66",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "cb8b0515efe9d62a1c36",
+    "url": "/movie-app/static/css/main.56823844.chunk.css"
+  },
+  {
+    "revision": "58fb522645607ebcb674",
+    "url": "/movie-app/static/js/2.a612a5fe.chunk.js"
+  },
+  {
+    "revision": "cb8b0515efe9d62a1c36",
+    "url": "/movie-app/static/js/main.2bd182db.chunk.js"
+  },
+  {
+    "revision": "2c9d5564c7fabe2c7e66",
+    "url": "/movie-app/static/js/runtime-main.2a14d7aa.js"
+  }
+]);
